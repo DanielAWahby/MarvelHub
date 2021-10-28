25c7c5384a250c3f58bcd43e75d6cc3b398c0168
Daniel Ashraf Gerges Wahby <danielashrafgerges@icloud.com>
2021-10-28 13:23:15 +0200

    Added Initial Data Model Implementation.

---
+++ Marvel Hub/Marvel Hub/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  Marvel Hub
+//
+//  Created by Daniel Wahby on 28/10/2021.
+//
+
+import Foundation
---
+++ Marvel Hub/Marvel Hub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel Hub
+//
+//  Created by Daniel Wahby on 28/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
