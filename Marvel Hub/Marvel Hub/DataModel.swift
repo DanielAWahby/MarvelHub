569b6e9f163070d664605e9e99488fe332efe4ce
Daniel Ashraf Gerges Wahby <danielashrafgerges@icloud.com>
2021-10-29 18:42:04 +0200

    Auto stash before merge of "main" and "origin/main"

--- Marvel Hub/Marvel Hub/DataModel.swift
+++ Marvel Hub/Marvel Hub/DataModel.swift
@@ -1,3 +1,4 @@
+
 //
 //  DataModel.swift
 //  Marvel Hub
@@ -6,3 +7,65 @@
 //
 
 import Foundation
+
+struct Character:Codable {
+    var id:Int?
+    var name:String?
+    var description:String?
+    var thumbnail:Image?
+    var comics:ComicList?
+    var stories:StoryList?
+    var events:EventList?
+    var series:SeriesList?
+}
+
+struct Image:Codable {
+    var path:String?
+    var imageExtension:String?
+}
+
+struct ComicList:Codable {
+    var available:Int?
+    var returned:Int?
+    var collectionURI:String?
+    var items:[ComicSummary]?
+}
+struct ComicSummary:Codable{
+    var resourceURI:String?
+    var name:String?
+}
+
+struct StoryList:Codable {
+    var available:Int?
+    var returned:Int?
+    var collectionURI:String?
+    var items:[StorySummary]?
+}
+struct StorySummary:Codable{
+    var resourceURI:String?
+    var name:String?
+    var type:String?
+}
+
+struct EventList:Codable {
+    var available:Int?
+    var returned:Int?
+    var collectionURI:String?
+    var items:[EventSummary]?
+}
+struct EventSummary:Codable{
+    var resourceURI:String?
+    var name:String?
+}
+
+struct SeriesList:Codable {
+    var available:Int?
+    var returned:Int?
+    var collectionURI:String?
+    var items:[SeriesSummary]?
+}
+
+struct SeriesSummary:Codable{
+    var resourceURI:String?
+    var name:String?
+}
