ed1b00e02a10e55dc843ec892c714caafbf988ba
Daniel Ashraf Gerges Wahby <danielashrafgerges@icloud.com>
2021-10-30 17:57:48 +0200

    Added Initial Character Retrieval functionality

---
+++ Marvel Hub/Marvel Hub/CharacterViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CharacterViewCell.swift
+//  Marvel Hub
+//
+//  Created by Daniel Wahby on 30/10/2021.
+//
+
+import UIKit
+
+class CharacterViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
